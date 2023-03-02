852fef65296553a1d7f6c85b515908ea0e60aec6
KausaChangs <changayakausa@gmail.com>
2023-03-02 14:40:15 +0200

    app is done

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const possibleChoices = document.querySelectorAll("button");
 
 let userChoice;
 let computerChoice;
+let result;
 
 possibleChoices.forEach((possibleChoice) =>
   possibleChoice.addEventListener("click", (e) => {
     userChoice = e.target.id;
     userChoiceDisplay.innerHTML = userChoice;
     generateComputerChoice();
+    getResult();
   })
 );
 
@@ -29,4 +31,32 @@ function generateComputerChoice() {
   }
 
   computerChoiceDisplay.innerHTML = computerChoice;
+}
+
+function getResult() {
+  if (computerChoice === userChoice) {
+    result = "its a draw!";
+  }
+
+  if (computerChoice === "rock" && userChoice === "paper") {
+    result = "you win!";
+  }
+
+  if (computerChoice === "paper" && userChoice === "scissors") {
+    result = "you win!";
+  }
+  if (computerChoice === "scissors" && userChoice === "rock") {
+    result = "you win!";
+  }
+  if (computerChoice === "paper" && userChoice === "rock") {
+    result = "you lost!";
+  }
+  if (computerChoice === "rock" && userChoice === "scissors") {
+    result = "you lost!";
+  }
+  if (computerChoice === "scissors" && userChoice === "paper") {
+    result = "you lost!";
+  }
+
+  resultDisplay.innerHTML = result;
 }
