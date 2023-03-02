425b0c9437f8170fb7981a9569c1944f0bbe40a5
KausaChangs <changayakausa@gmail.com>
2023-03-02 12:32:13 +0200

    added generateComputerChoice() logic

--- app.js
+++ app.js
@@ -4,10 +4,27 @@ const resultDisplay = document.getElementById("result");
 const possibleChoices = document.querySelectorAll("button");
 
 let userChoice;
+let computerChoice;
 
 possibleChoices.forEach((possibleChoice) =>
   possibleChoice.addEventListener("click", (e) => {
     userChoice = e.target.id;
     userChoiceDisplay.innerHTML = userChoice;
+    generateComputerChoice();
   })
 );
+
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1; //or you can use 3
+  console.log(randomNumber);
+
+  if (randomNumber === 1) {
+    computerChoice = "rock";
+  }
+  if (randomNumber === 2) {
+    computerChoice = "paper";
+  }
+  if (randomNumber === 3) {
+    computerChoice = "scissors";
+  }
+}
